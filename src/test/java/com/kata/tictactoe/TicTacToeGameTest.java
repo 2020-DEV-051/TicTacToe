0b3f947b45ca5af6711b381d338060c2bc1cf587
Lokesh Varshney <var.lokesh@gmail.com>
2020-03-02 23:46:47 +0530

    Added static import for assertThat and is methods

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -1,14 +1,15 @@
 package com.kata.tictactoe;
 
-import org.hamcrest.core.Is;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class TicTacToeGameTest {
 
     @Test
     public void ticTacToeGameShouldReturn_GameIsContinueMessage_IfDefaultPlayerX_Makes1stMoveAtIndex_00() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        Assert.assertThat(ticTacToeGame.playGame(0,0), Is.is("Game is Continue!"));
+        assertThat(ticTacToeGame.playGame(0,0), is("Game is Continue!"));
     }
 }
