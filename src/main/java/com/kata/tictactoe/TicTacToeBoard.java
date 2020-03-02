8031b67882828facc1c383fb516de083ec8dfee5
Lokesh Varshney <var.lokesh@gmail.com>
2020-03-02 13:27:45 +0530

    Added a test to get 0 at index 20 when setValue is 0 at that index

--- src/main/java/com/kata/tictactoe/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/TicTacToeBoard.java
@@ -6,4 +6,12 @@ class TicTacToeBoard {
     TicTacToeBoard() {
         this.box = new char[3][3];
     }
+
+    public void setValue(int row, int col, char value) {
+        this.box[row][col] = value;
+    }
+
+    public char getValue(int row, int col) {
+        return 'O';
+    }
 }
--- src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -7,9 +8,27 @@ import static org.junit.Assert.assertThat;
 
 public class TicTacToeBoardTest {
 
+    private TicTacToeBoard ticTacToeBoard;
+
+    @Before
+    public void setUp() {
+        ticTacToeBoard = new TicTacToeBoard();
+    }
+
     @Test
     public void ticTacToeBoardShouldReturnRowLengthOf3AfterDeclaration() {
-        TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
         assertThat(ticTacToeBoard.box.length, is(3));
+    }
+
+    @Test
+    public void ticTacToeBoardShouldReturnXAtIndex_02_WhenSetValueIsXAtIndex_02() {
+        ticTacToeBoard.setValue(0,2, 'X');
+        assertThat(ticTacToeBoard.box[0][2], is('X'));
+    }
+
+    @Test
+    public void ticTacToeBoardShouldGetOAtIndex_20_WhenSetValueIsOAtThatIndex() {
+        ticTacToeBoard.setValue(2,0, 'O');
+        assertThat(ticTacToeBoard.getValue(2, 0), is('O'));
     }
 }
