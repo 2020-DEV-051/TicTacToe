37c908d5f209181c113464dee4946df8eedee15b
Lokesh Varshney <var.lokesh@gmail.com>
2020-03-02 14:07:25 +0530

    Updated scope for isPlayedPosition method to package-private

--- src/main/java/com/kata/tictactoe/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/TicTacToeBoard.java
@@ -17,7 +17,7 @@ class TicTacToeBoard {
         return this.box[row][col];
     }
 
-    public boolean isPlayedPosition(int row, int col) {
+    boolean isPlayedPosition(int row, int col) {
         return this.getValue(row, col) == X || this.getValue(row,col) == O;
     }
 }
