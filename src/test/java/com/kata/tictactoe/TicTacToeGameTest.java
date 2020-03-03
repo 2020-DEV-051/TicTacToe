3d3c7ec4d168a6853f790e3c408fd393909c8c6e
Lokesh Varshney <var.lokesh@gmail.com>
2020-03-03 12:04:30 +0530

    Extracted constnat for Player O plays on the played position as PLAYER_O_PLAYS_ON_THE_PLAYED_POSITION in test class

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertThat;
 public class TicTacToeGameTest {
 
     private static final String GAME_IS_CONTINUE = "Game is Continue!";
+    private static final String PLAYER_O_PLAYS_ON_THE_PLAYED_POSITION = "Player O plays on the played position.";
 
     @Test
     public void ticTacToeGameShouldReturn_GameIsContinueMessage_IfDefaultPlayerX_Makes1stMoveAtIndex_00() {
@@ -19,6 +20,6 @@ public class TicTacToeGameTest {
     public void ticTacToeGameShouldReturn_PositionMessage_IfPlayerOPlaysOnPlayedPosition() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,1), is(GAME_IS_CONTINUE));
-        assertThat(ticTacToeGame.playGame(0,1), is("Player O plays on the played position."));
+        assertThat(ticTacToeGame.playGame(0,1), is(PLAYER_O_PLAYS_ON_THE_PLAYED_POSITION));
     }
 }
