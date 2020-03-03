41d12317ce46b7b7c0b80c48ff8e5a10a0ee2a3b
Lokesh Varshney <var.lokesh@gmail.com>
2020-03-03 12:06:12 +0530

    Moved ticTacToeGame local variable as a field variable and initialized it in setUp method

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -9,16 +10,20 @@ public class TicTacToeGameTest {
 
     private static final String GAME_IS_CONTINUE = "Game is Continue!";
     private static final String PLAYER_O_PLAYS_ON_THE_PLAYED_POSITION = "Player O plays on the played position.";
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void setUp() {
+        ticTacToeGame = new TicTacToeGame();
+    }
 
     @Test
     public void ticTacToeGameShouldReturn_GameIsContinueMessage_IfDefaultPlayerX_Makes1stMoveAtIndex_00() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0), is(GAME_IS_CONTINUE));
     }
 
     @Test
     public void ticTacToeGameShouldReturn_PositionMessage_IfPlayerOPlaysOnPlayedPosition() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,1), is(GAME_IS_CONTINUE));
         assertThat(ticTacToeGame.playGame(0,1), is(PLAYER_O_PLAYS_ON_THE_PLAYED_POSITION));
     }
